a9b7e9067ac89de2cfc810024f4c53c3e22093ff
Ananya <ananya.0102@gmail.com>
2022-10-15 21:28:42 +0530

    init project

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Ananya</h1>
+// } //functional component
+
+//export const Greet = () => <h1>Hello Ananya</h1> //named export
+
+const Greet = () => <h1>Hello Ananya</h1> //arrow functions
+
+export default Greet //default export
